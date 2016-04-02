5139d65018bd9e888dfd6fe4dfc198270aab8a03
DennisCarcamo <joel.carcamos@unitec.edu>
2016-04-01 18:38:30 -0600

    Finalizado

---
+++ node.cpp
@@ -0,0 +1,43 @@
+#include <string>
+#include "person.hpp"
+#include "node.hpp"
+
+using namespace std;
+
+	Node::Node(){
+	}
+	
+	Node::Node(Person persona):persona(persona){
+		next=new Node();
+		setValue(persona);
+	}
+
+	Node::~Node(){
+		delete next;
+	}
+	
+	Person Node::getValue(){
+		return persona;
+	}
+	
+	Node* Node::getNext(){
+		return next;
+	}
+	
+	void Node::setValue(Person persona){
+		this->persona.setName(persona.getName());
+		this->persona.setPhone(persona.getPhone());
+	}
+	
+	void Node::setNext(Node* next){
+		this-> next=next;
+	}
+
+	bool Node::hasNext(){
+		if (next!=NULL){
+			return true;
+		}else{
+			return false;
+		}
+	}
+ 
